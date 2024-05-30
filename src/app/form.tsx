96cae2ea8c512296b62cbd7a44d158d0f70b22fb
Rohit C <rcvium24@gmail.com>
2024-05-31 03:31:27 +0530

    Uploaded files

---
+++ src/app/form.tsx
@@ -0,0 +1,215 @@
+"use client"
+import { FormEvent, useState } from "react";
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+};
+
+const safetySettings = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+];
+
+export default function Form({ GEMINI_API_KEY }: any) {
+    const [input, setInput] = useState<string | null>(null)
+    const [output, setOutput] = useState<string | null>(null)
+
+    const info={
+        name:"Rohit C",
+        age:(new Date().getFullYear())-2002,
+        email:"rohitc21official@gmail.com",
+        location:"Thrissur, Kerala, India",
+        qualification:"B.Tech Computer Science and Engineering",
+        education:{
+            college:{
+                name:"Vidya Academy of Science and Technology, Thrissur",
+                university:"APJ Abdul Kalam Technological University",
+                branch:"Computer Science and Engineering",
+                duration:"2020-2024",
+                cgpa:8.43
+            },
+            school:{
+                name:"Hari Sri Vidya Nidhi School, Thrissur",
+                hsc:{
+                    board:"ISC",
+                    stream:"PCM + Computer Science",
+                    duration:"2019-2020",
+                    percentage:"89%"
+                },
+                ssc:{
+                    board:"ICSE",
+                    stream:"Science",
+                    duration:"2017-2018",
+                    percentage:"93%"
+                }
+            }
+        },
+        experience:{
+            mulearn:{
+                role:"Technical Documentation",
+                organization:"GTech MuLearn",
+                duration:"2023-2024"
+            },
+            gdsc:{
+                role:"Member",
+                organization:"Google Developer Students Club",
+                duration:"2022-2024"
+            }
+        },
+        skills:{
+            technical:"C, Java, Python, Solidity, HTML, CSS, PHP, JavaScript, TypeScript, React.js, Next.js, Django, MySQL, PostgreSQL, Figma, Canva, Vercel, Postman, Git, GitHub",
+            soft:"Critical thinking, Creative thinking, Problem solving, Self-learning, Time management, Adaptability, Team player, Leadership"
+        },
+        interests:"Frontend development, Backend development, UI designing, AI/ML, LLM",
+        certificates:"Infosys Springboard Artificial Primer Certification, Google IT Automation with Python, Google Cloud Study Jam, Cisco Cybersecurity Essentials, Manifold Advanced Python Programming for Competitive Coding, IIT Bombay PHP and MySQL Training",
+        projects:{
+            SmartPricing:{
+                name:"AI Powered Weighing and Pricing Software for Fruits and Vegetables",
+                description:"A system that uses computer vision to identify the type of fruits or vegetable placed on the weighing scale and automatically generates the price corresponding to its weight.",
+                stack:"Python, Tkinter, MySQL",
+                repository:"https://github.com/RO21T/MINI"
+            },
+            GIV3R:{
+                name:"GIV3R - The Decentralized Charity Portal",
+                description:"A DApp that promotes transparent philanthropy by integrating donors, beneficiaries, service providers and NGOs with the Blockchain network for donations and fundraisers.",
+                stack:"Next.js, Solidty, Metamask, PostgreSQL",
+                repository:"https://github.com/RO21T/GIV3R"
+            },
+            WARECARE255:{
+                name:"WARECARE(255) - Warehouse Database Voice Assistant",
+                description:"A personalized voice assistant to query warehouse databases in natural language to perform CRUD operations on its tables with built-in access controls",
+                stack:"Python, Langchain, Gemini, PostgreSQL",
+                repository:"https://github.com/RO21T/WARECARE-255"
+            },
+            Resumeland:{
+                name:"Resumeland - Personalized Resume Generator",
+                description:"A non-conventional resume generator where the user can enter all details during registration and generate resumes tailor-made for the job by providing just the link to the job description.",
+                stack:"Django, Selenium, Gemini, PostgreSQL",
+                repository:"https://github.com/RO21T/RESUMELAND"
+            },
+            XTRKT:{
+                name:"XTRKT - Scientific Document Parser",
+                description:"A web app that extracts details from scientific documents and generates information related to it with options to edit and store the data and also convert it to excel format.",
+                stack:"Django, Tesseract OCR, Gemini, MySQL",
+                repository:"https://github.com/RO21T/XTRKT"
+            },
+            Blink:{
+                name:"Blink to Morse Code Converter and Translator",
+                description:"A web app that converts eye blinks to dots and dashes of Morse code and translates it to English text in real-time.",
+                stack:"Django, OpenCV, Dlib",
+                repository:"https://github.com/RO21T/BLINKv1"
+            },
+            Todo:{
+                name:"Todo App",
+                description:"A simple to-do app to add, edit, delete and strike-off tasks leveraging local storage options.",
+                stack:"React",
+                repository:"https://github.com/RO21T/TODO-REACT"
+            },
+            API:{
+                name:"Todo API",
+                description:"A simple to-do API to create, update, delete and view tasks and deadlines enhanced by authentication and database storage.",
+                stack:"Django REST Framework, MySQL",
+                repository:"https://github.com/RO21T/TODO-DJANGO"
+            },
+            Dice:{
+                name:"Dice App",
+                description:"An app to roll a dice.",
+                repository:"https://github.com/RO21T/DICE"
+            }
+        },
+        hobbies:"Story writing, Versification, Meme creation, Quiz, Debate"
+    }
+
+    const apiKey = GEMINI_API_KEY;
+    const genAI = new GoogleGenerativeAI(apiKey);
+
+    const model = genAI.getGenerativeModel({
+    model: "gemini-1.0-pro",
+    });
+
+    const chatSession = model.startChat({
+        generationConfig,
+        safetySettings,
+        history: [
+        ],
+    });
+
+    const handleSubmit=async (e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        const formData=new FormData(e.currentTarget)
+        const query=formData.get("query") as string
+        if(query){
+            const result = await chatSession.sendMessage(`
+                Listen carefully!
+                You are not Gemini anymore.
+                You should act like the person mentioned in ${Object.entries(info)}.
+                Answer ${query} like he would do choosing only the appropriate information from ${Object.entries(info)} based on the question asked.}.
+            `);
+            setInput(query)
+            setOutput(result.response.text())
+        }
+    }
+
+    if(input && output){
+        return(
+            <form className="form" onSubmit={handleSubmit}>
+            <div className="output">
+                <div className="question">
+                    <p>{input}</p>
+                </div>
+                <div className="answer">
+                    <p>{output}</p>
+                </div>
+            </div>
+            <div className="input">
+                <div className="textbox">
+                    <div className="space"></div>
+                    <div className="type">
+                        <input type="text" className="query" name="query" placeholder="Let's have a chat about me!" autoComplete="off"/>
+                    </div>
+                </div>
+                <div className="button">
+                    <input type="submit" className="ask" value={""}/>
+                </div>
+            </div>
+            </form>
+        )
+    }
+    else{
+        return(
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="output"></div>
+                <div className="input">
+                    <div className="textbox">
+                        <div className="space"></div>
+                        <div className="type">
+                            <input type="text" className="query" name="query" placeholder="Let's have a chat about me!" autoComplete="off"/>
+                        </div>
+                    </div>
+                    <div className="button">
+                        <input type="submit" className="ask" value={""}/>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+import Form from "./form";
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 export default function Home() {
   return (
@@ -16,7 +20,7 @@ export default function Home() {
         </div>
         <div className="bottom">
           <div className="description">
-            <p>An enthusiastic and goal-driven Computer Science Engineering student with proficient team player abilities with adequate knowledge in mainstream programming languages seeking for an opportunity to put his potential to full use in the field of Software Development.</p>
+            <p>An enthusiastic and goal-driven Computer Science and Engineering student with proficient team player abilities with adequate knowledge of mainstream programming languages seeking an opportunity to put his potential to full use in the field of Software Development.</p>
           </div>
           <div className="social">
             <div className="one">
@@ -25,7 +29,7 @@ export default function Home() {
               </Link>
             </div>
             <div className="two">
-              <Link className="link2" href={'https://www.linkedin.com/in/rohit-c-6aa329255/'}>
+              <Link className="link2" href={'https://www.linkedin.com/in/c-rohit'}>
                 <Image className="linkedin" priority={true} src={"/images/linkedin.png"} width={50} height={45} alt="LinkedIn"/>
               </Link>
             </div>
@@ -37,7 +41,14 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="up">
+          <h1>Portfolio GPT</h1>
+        </div>
+        <div className="down">
+          <Form GEMINI_API_KEY={process.env.GEMINI_API_KEY}/>
+        </div>
+      </div>
     </>
   );
 }
