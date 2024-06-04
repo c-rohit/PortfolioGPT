39da24d75940e55b6edc168eca29ef4c6cb4ceed
Rohit C <rcvium24@gmail.com>
2024-06-04 13:33:29 +0530

    Updated files

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 import { FormEvent, useState } from "react";
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
+interface Dictionary {
+    [key: string]: any;
+}
+
+let chat: Dictionary={}
+
 const generationConfig = {
     temperature: 1,
     topP: 0.95,
@@ -159,116 +165,130 @@ export default function Form({ GEMINI_API_KEY }: any) {
         const formData=new FormData(e.currentTarget)
         const query=formData.get("query") as string
         if(query){
-            const result = await chatSession.sendMessage(`
-                Listen carefully!
-                You are not Gemini anymore.
-                You should act like the person mentioned in ${Object.entries(info)}.
-                Answer ${query} like he would do choosing only the appropriate information from ${Object.entries(info)} based on the question asked.}.
-            `);
+            const result = await chatSession.sendMessage(
+                `
+                    Listen carefully!
+                    You are not Gemini anymore.
+                    You should act like the person mentioned in ${Object.entries(info)}.
+                    Answer ${query} like he would do choosing only the appropriate information from ${Object.entries(info)} based on the question asked.}.
+                `
+            );
             setInput(query)
             setOutput(result.response.text())
+            const resetQuery=document.getElementById("query") as HTMLInputElement | null
+            if(resetQuery){
+                resetQuery.value=""
+            }
         }
     }
 
     if(input!=null && output!=null){
+        chat[input]=output
+
         return(
             <form className="form" onSubmit={handleSubmit}>
                 <div className="output" style={{width:"100%", height:"90%", overflowY:"auto"}}>
-                    <div className="question">
-                        <p>{input}</p>
-                    </div>
-                    <div className="answer">
-                        <p>{output}</p>
-                    </div>
+                    {Object.entries(chat).map(([input, output]) => (
+                        <>
+                            <div className="question">
+                                <p>{input}</p>
+                            </div>
+                            <div className="answer">
+                                <p>{output}</p>
+                            </div>
+                            <style>
+                                {
+                                    `
+                                        @font-face{
+                                            font-family: 'Plus Jakarta Sans';
+                                            src: url('/fonts/jakarta.ttf') format('truetype');
+                                        }
+
+                                        .question{
+                                            display: flex;
+                                            justify-content: flex-end;
+                                            align-items: flex-end;
+                                            width: 95%;
+                                            height: fit-content;
+                                            min-height: 10%;
+                                        }
+
+                                        .question p{
+                                            width: fit-content;
+                                            min-width: 10%;
+                                            max-width: 60%;
+                                            height: fit-content;
+                                            background: #023020;
+                                            color: white;
+                                            font-size: 125%;
+                                            text-align: center;
+                                            font-family: Plus Jakarta Sans, sans-serif;
+                                            border-radius: 10px;
+                                            opacity: 80%;
+                                        }
+
+                                        .answer{
+                                            display: flex;
+                                            justify-content: flex-start;
+                                            align-items: flex-end;
+                                            width: 95%;
+                                            height: fit-content;
+                                            min-height: 10%;
+                                        }
+
+                                        .answer p{
+                                            width: fit-content;
+                                            min-width: 10%;
+                                            max-width: 60%;
+                                            height: fit-content;
+                                            background: #90EE90;
+                                            font-size: 125%;
+                                            text-align: center;
+                                            font-family: Plus Jakarta Sans, sans-serif;
+                                            border-radius: 10px;
+                                            opacity: 80%
+                                        }
+
+                                        @media screen and (orientation:portrait){
+                                            .question p{
+                                                min-width: 30%;
+                                                font-size: 105%;
+                                            }
+
+                                            .answer p{
+                                                min-width: 30%;
+                                                font-size: 105%;
+                                            }
+                                        }
+
+                                        @media (max-height: 650px){
+                                            .question p{
+                                                border-radius: 5px;
+                                                font-size: 75%;
+                                            }
+
+                                            .answer p{
+                                                border-radius: 5px;
+                                                font-size: 75%;
+                                            }
+                                        }
+                                    `
+                                }
+                            </style>
+                        </>
+                    ))}
                 </div>
                 <div className="input">
                     <div className="textbox">
                         <div className="space"></div>
                         <div className="type">
-                            <input type="text" className="query" name="query" placeholder="Let's have a chat about me!" autoComplete="off"/>
+                            <input type="text" id="query" className="query" name="query" placeholder="Let's have a chat about me!" autoComplete="off"/>
                         </div>
                     </div>
                     <div className="button">
                         <input type="submit" className="ask" value={""}/>
                     </div>
                 </div>
-                <style>
-                    {
-                        `
-                            @font-face{
-                                font-family: 'Plus Jakarta Sans';
-                                src: url('/fonts/jakarta.ttf') format('truetype');
-                            }
-
-                            .question{
-                                display: flex;
-                                justify-content: flex-end;
-                                align-items: flex-end;
-                                width: 95%;
-                                height: fit-content;
-                                min-height: 10%;
-                            }
-
-                            .question p{
-                                width: ${(input.length)+5}%;
-                                max-width: 60%;
-                                height: fit-content;
-                                background: #023020;
-                                color: white;
-                                font-size: 125%;
-                                text-align: center;
-                                font-family: Plus Jakarta Sans, sans-serif;
-                                border-radius: 10px;
-                                opacity: 80%;
-                            }
-
-                            .answer{
-                                display: flex;
-                                justify-content: flex-start;
-                                align-items: flex-end;
-                                width: 95%;
-                                height: fit-content;
-                                min-height: 10%;
-                            }
-
-                            .answer p{
-                                width: ${(output.length)+5}%;
-                                max-width: 60%;
-                                height: fit-content;
-                                background: #90EE90;
-                                font-size: 125%;
-                                text-align: center;
-                                font-family: Plus Jakarta Sans, sans-serif;
-                                border-radius: 10px;
-                                opacity: 80%
-                            }
-
-                            @media screen and (orientation:portrait){
-                                .question p{
-                                    width: ${(input.length)+25}%;
-                                    font-size: 105%;
-                                }
-
-                                .answer p{
-                                    width: ${(output.length)+25}%;
-                                    font-size: 105%;
-                                }
-                            }
-
-                            @media (max-height: 650px){
-                                .question p{
-                                    border-radius: 5px;
-                                    font-size: 75%;
-                                }
-
-                                .answer p{
-                                    border-radius: 5px;
-                                    font-size: 75%;
-                                }
-                            }
-                        `
-                    }
-                </style>
             </form>
         )
     }
