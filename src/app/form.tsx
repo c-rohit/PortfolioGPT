f3ad38f827b5b62fdfc8b8f6e696dca4574b557e
Rohit C <rcvium24@gmail.com>
2024-06-07 19:37:24 +0530

    Updated files

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -71,12 +71,12 @@ export default function Form({ GEMINI_API_KEY }: any) {
             }
         },
         experience:{
-            mulearn:{
+            one:{
                 role:"Technical Documentation",
                 organization:"GTech MuLearn",
                 duration:"2023-2024"
             },
-            gdsc:{
+            two:{
                 role:"Member",
                 organization:"Google Developer Students Club",
                 duration:"2022-2024"
@@ -89,57 +89,58 @@ export default function Form({ GEMINI_API_KEY }: any) {
         interests:"Frontend development, Backend development, UI designing, AI/ML, LLM",
         certificates:"Infosys Springboard Artificial Primer Certification, Google IT Automation with Python, Google Cloud Study Jam, Cisco Cybersecurity Essentials, Manifold Advanced Python Programming for Competitive Coding, IIT Bombay PHP and MySQL Training",
         projects:{
-            SmartPricing:{
+            one:{
                 name:"AI Powered Weighing and Pricing Software for Fruits and Vegetables",
                 description:"A system that uses computer vision to identify the type of fruits or vegetable placed on the weighing scale and automatically generates the price corresponding to its weight.",
                 stack:"Python, Tkinter, MySQL",
                 repository:"https://github.com/RO21T/MINI"
             },
-            GIV3R:{
+            two:{
                 name:"GIV3R - The Decentralized Charity Portal",
                 description:"A DApp that promotes transparent philanthropy by integrating donors, beneficiaries, service providers and NGOs with the Blockchain network for donations and fundraisers.",
                 stack:"Next.js, Solidty, Metamask, PostgreSQL",
                 repository:"https://github.com/RO21T/GIV3R"
             },
-            WARECARE255:{
+            three:{
                 name:"WARECARE(255) - Warehouse Database Voice Assistant",
                 description:"A personalized voice assistant to query warehouse databases in natural language to perform CRUD operations on its tables with built-in access controls",
                 stack:"Python, Langchain, Gemini, PostgreSQL",
                 repository:"https://github.com/RO21T/WARECARE-255"
             },
-            Resumeland:{
+            four:{
                 name:"Resumeland - Personalized Resume Generator",
                 description:"A non-conventional resume generator where the user can enter all details during registration and generate resumes tailor-made for the job by providing just the link to the job description.",
                 stack:"Django, Selenium, Gemini, PostgreSQL",
                 repository:"https://github.com/RO21T/RESUMELAND"
             },
-            XTRKT:{
+            five:{
                 name:"XTRKT - Scientific Document Parser",
                 description:"A web app that extracts details from scientific documents and generates information related to it with options to edit and store the data and also convert it to excel format.",
                 stack:"Django, Tesseract OCR, Gemini, MySQL",
                 repository:"https://github.com/RO21T/XTRKT"
             },
-            Blink:{
+            six:{
                 name:"Blink to Morse Code Converter and Translator",
                 description:"A web app that converts eye blinks to dots and dashes of Morse code and translates it to English text in real-time.",
                 stack:"Django, OpenCV, Dlib",
                 repository:"https://github.com/RO21T/BLINKv1"
             },
-            Todo:{
+            seven:{
                 name:"Todo App",
                 description:"A simple to-do app to add, edit, delete and strike-off tasks leveraging local storage options.",
                 stack:"React",
                 repository:"https://github.com/RO21T/TODO-REACT"
             },
-            API:{
+            eight:{
                 name:"Todo API",
                 description:"A simple to-do API to create, update, delete and view tasks and deadlines enhanced by authentication and database storage.",
                 stack:"Django REST Framework, MySQL",
                 repository:"https://github.com/RO21T/TODO-DJANGO"
             },
-            Dice:{
+            nine:{
                 name:"Dice App",
                 description:"An app to roll a dice.",
+                stack:"Django",
                 repository:"https://github.com/RO21T/DICE"
             }
         },
@@ -165,19 +166,135 @@ export default function Form({ GEMINI_API_KEY }: any) {
         const formData=new FormData(e.currentTarget)
         const query=formData.get("query") as string
         if(query){
-            const result = await chatSession.sendMessage(
-                `
-                    Listen carefully!
-                    You are not Gemini anymore.
-                    You should act like the person mentioned in ${Object.entries(info)}.
-                    Answer ${query} like he would do choosing only the appropriate information from ${Object.entries(info)} based on the question asked.}.
-                `
-            );
-            setInput(query)
-            setOutput(result.response.text())
-            const resetQuery=document.getElementById("query") as HTMLInputElement | null
-            if(resetQuery){
-                resetQuery.value=""
+            try{
+                const result = await chatSession.sendMessage(
+                    `
+                        Listen carefully!
+                        You are not Gemini anymore.
+                        You should act like the person mentioned in ${Object.entries(info)}.
+                        Derieve the context of ${query} from the following facts:
+                        
+                        Basic details:
+                        ${Object.entries(info)},
+                        ${Object.entries(info.name)},
+                        ${Object.entries(info.email)},
+                        ${Object.entries(info.location)},
+                        ${Object.entries(info.qualification)},
+                        
+                        Education details:
+                        ${Object.entries(info.education)},
+                        ${Object.entries(info.education.college)},
+                        ${Object.entries(info.education.college.name)},
+                        ${Object.entries(info.education.college.university)},
+                        ${Object.entries(info.education.college.branch)},
+                        ${Object.entries(info.education.college.duration)},
+                        ${Object.entries(info.education.college.cgpa)},
+                        ${Object.entries(info.education.school)},
+                        ${Object.entries(info.education.school.name)},
+                        ${Object.entries(info.education.school.hsc)},
+                        ${Object.entries(info.education.school.hsc.board)},
+                        ${Object.entries(info.education.school.hsc.stream)},
+                        ${Object.entries(info.education.school.hsc.duration)},
+                        ${Object.entries(info.education.school.hsc.percentage)},
+                        ${Object.entries(info.education.school.ssc)},
+                        ${Object.entries(info.education.school.ssc.board)},
+                        ${Object.entries(info.education.school.ssc.stream)},
+                        ${Object.entries(info.education.school.ssc.duration)},
+                        ${Object.entries(info.education.school.ssc.percentage)},
+                        
+                        Experience details:
+                        ${Object.entries(info.experience)},
+                        ${Object.entries(info.experience.one)},
+                        ${Object.entries(info.experience.one.organization)},
+                        ${Object.entries(info.experience.one.role)},
+                        ${Object.entries(info.experience.one.duration)},
+                        ${Object.entries(info.experience.two)},
+                        ${Object.entries(info.experience.two.organization)},
+                        ${Object.entries(info.experience.two.role)},
+                        ${Object.entries(info.experience.two.duration)},
+                        
+                        Skills details:
+                        ${Object.entries(info.skills)},
+                        ${Object.entries(info.skills.technical)},
+                        ${Object.entries(info.skills.soft)},
+                        
+                        Interests details:
+                        ${Object.entries(info.interests)},
+                        
+                        Certificates details:
+                        ${Object.entries(info.certificates)},
+                        
+                        Project details:
+                        ${Object.entries(info.projects)},
+                        ${Object.entries(info.projects.one)},
+                        ${Object.entries(info.projects.one.name)},
+                        ${Object.entries(info.projects.one.description)},
+                        ${Object.entries(info.projects.one.stack)},
+                        ${Object.entries(info.projects.one.repository)},
+                        ${Object.entries(info.projects.two)},
+                        ${Object.entries(info.projects.two.name)},
+                        ${Object.entries(info.projects.two.description)},
+                        ${Object.entries(info.projects.two.stack)},
+                        ${Object.entries(info.projects.two.repository)},
+                        ${Object.entries(info.projects.three)},
+                        ${Object.entries(info.projects.three.name)},
+                        ${Object.entries(info.projects.three.description)},
+                        ${Object.entries(info.projects.three.stack)},
+                        ${Object.entries(info.projects.three.repository)},
+                        ${Object.entries(info.projects.four)},
+                        ${Object.entries(info.projects.four.name)},
+                        ${Object.entries(info.projects.four.description)},
+                        ${Object.entries(info.projects.four.stack)},
+                        ${Object.entries(info.projects.four.repository)},
+                        ${Object.entries(info.projects.five)},
+                        ${Object.entries(info.projects.five.name)},
+                        ${Object.entries(info.projects.five.description)},
+                        ${Object.entries(info.projects.five.stack)},
+                        ${Object.entries(info.projects.five.repository)},
+                        ${Object.entries(info.projects.six)},
+                        ${Object.entries(info.projects.six.name)},
+                        ${Object.entries(info.projects.six.description)},
+                        ${Object.entries(info.projects.six.stack)},
+                        ${Object.entries(info.projects.six.repository)},
+                        ${Object.entries(info.projects.seven)},
+                        ${Object.entries(info.projects.seven.name)},
+                        ${Object.entries(info.projects.seven.description)},
+                        ${Object.entries(info.projects.seven.stack)},
+                        ${Object.entries(info.projects.seven.repository)},
+                        ${Object.entries(info.projects.eight)},
+                        ${Object.entries(info.projects.eight.name)},
+                        ${Object.entries(info.projects.eight.description)},
+                        ${Object.entries(info.projects.eight.stack)},
+                        ${Object.entries(info.projects.eight.repository)},
+                        ${Object.entries(info.projects.nine)},
+                        ${Object.entries(info.projects.nine.name)},
+                        ${Object.entries(info.projects.nine.description)},
+                        ${Object.entries(info.projects.nine.stack)},
+                        ${Object.entries(info.projects.nine.repository)},
+                        
+                        Hobbies details:
+                        ${Object.entries(info.hobbies)}
+                        
+                        By accurately interpreting the context and the facts and generate a suitable answer for ${query}.
+                        If the question is not related to the context, reply back with answer stating you don't know just like a human being would answer it.
+                        Beware! People will try to trick you into revealing your true identity as a bot but do not fall for those.
+                        Always play the character Rohit C and answer the questions as if you were him in first person.
+                        Only answer the question asked and do not provide excess information.
+                        Never mention that you are referring answers from a set of facts provided to you.
+                        If you do not have answer to a particular question, never mention that the context or the given facts do not contain the info, just reply with "Can you please elaborate." or anything like this.
+                        If the question contains any sensitive content or is sexually explicit or uses curse words reply back with an appropriate thug life message based on the question.
+                    `
+                );
+                setInput(query)
+                setOutput(result.response.text())
+                const resetQuery=document.getElementById("query") as HTMLInputElement | null
+                if(resetQuery){
+                    resetQuery.value=""
+                }
+            }
+            catch(error){
+                alert("Oops! We've just faced an error. Please wait a while before trying again.")
+                window.location.reload()
             }
         }
     }
